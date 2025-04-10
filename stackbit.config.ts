7a4040a06c1264742fea21e47658c2395793cb35
ClearChoicePools <thomas.soccerdude@gmail.com>
2025-04-10 17:54:53 -0500

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,19 +1,9 @@
 import { defineStackbitConfig } from '@stackbit/types';
-
-export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "nextjs",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
-// stackbit.config.ts
-import { defineStackbitConfig } from "@stackbit/types";
-
-export default defineStackbitConfig({
-  stackbitVersion: "~0.6.0",
-  contentSources: [
-    // ...
-  ],
-  modelExtensions: [{ name: "page", type: "page", urlPath: "/{slug}" }]
-});
+ 
+ export default defineStackbitConfig({
+     "stackbitVersion": "~0.6.0",
+     "nodeVersion": "18",
+     "ssgName": "nextjs",
+     "contentSources": [],
+     "postInstallCommand": "npm i --no-save @stackbit/types"
+ })
