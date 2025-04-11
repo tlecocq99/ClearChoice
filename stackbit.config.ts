271b4ac65955ad8aeae9dcd4dfaf4157cc0e8183
ClearChoicePools <thomas.soccerdude@gmail.com>
2025-04-10 19:25:06 -0500

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,10 +1,1 @@
-// stackbit.config.ts
-import { defineStackbitConfig } from "@stackbit/types";
 
-export default defineStackbitConfig({
-  stackbitVersion: "~0.6.0",
-  contentSources: [
-    // ...
-  ],
-  modelExtensions: [{ name: "page", type: "page", urlPath: "/{slug}" }]
-});
