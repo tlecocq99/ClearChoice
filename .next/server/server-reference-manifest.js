38bf47445086382bca2760b3fb77f3cc06e48586
ClearChoicePools <thomas.soccerdude@gmail.com>
2025-04-17 17:21:22 -0500

    fixes

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,2 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1ICy9WMXxmERLiT0IQdIZ36SbXSzUzAM0/Jy/rPOjB0=\"\n}"
+self.__RSC_SERVER_MANIFEST =
+  '{\n  "node": {},\n  "edge": {},\n  "encryptionKey": "1ICy9WMXxmERLiT0IQdIZ36SbXSzUzAM0/Jy/rPOjB0="\n}';
