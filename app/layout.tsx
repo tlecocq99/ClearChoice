0288e1f8fa206d9528a221c333d4eee5b5e536f9
Thomas Lecocq <tlecocq99@gmail.com>
2025-09-18 22:45:36 -0500

    addition of favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/images/logo.png" />
       <body>
         {/* LocalBusiness structured data for improved local SEO */}
         <SEOJsonLd />
